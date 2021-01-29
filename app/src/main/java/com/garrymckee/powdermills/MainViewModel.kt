fed1a01a5818cedd6cf257570250266ea42add1b
garrymckee <gmk0232@gmail.com>
2021-01-29 01:05:23 +0000

    Integrate Mapbox permissions and centering on user location

---
+++ app/src/main/java/com/garrymckee/powdermills/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.garrymckee.powdermills
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.garrymckee.powdermills.ui.Event
+
+class MainViewModel @ViewModelInject constructor() : ViewModel() {
+    val locationPermission = MutableLiveData<Event<Boolean>>()
+    val shouldCheckLocationPermissions = MutableLiveData<Event<Boolean>>(Event(false))
+
+    fun setLocationPermission(locationPermissionGranted: Boolean) {
+        locationPermission.value = Event(locationPermissionGranted)
+    }
+
+    fun checkLocationPermissions() {
+        shouldCheckLocationPermissions.value = Event(true)
+    }
+}
--- app/src/main/java/com/garrymckee/powdermills/ui/map/MapFragment.kt
+++ app/src/main/java/com/garrymckee/powdermills/ui/map/MapFragment.kt
@@ -1,17 +1,25 @@
 package com.garrymckee.powdermills.ui.map
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.garrymckee.powdermills.MainViewModel
 import com.garrymckee.powdermills.R
 import com.garrymckee.powdermills.databinding.FragmentMapBinding
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.Symbol
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
@@ -22,14 +30,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 const val ICON_ID = "mapMarkerIconID"
-const val LAYER_ID = "mapLayerID"
-const val SOURCE_ID = "mapSourceID"
 
 @AndroidEntryPoint
 class MapFragment : Fragment() {
 
     private lateinit var symbolManager: SymbolManager
-    private lateinit var map: MapView
+    private lateinit var binding: FragmentMapBinding
+    private lateinit var mapView: MapView
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var style: Style
+    private val activityViewModel: MainViewModel by activityViewModels()
     private val viewModel: MapViewModel by viewModels()
     private var symbol: Symbol? = null
 
@@ -39,13 +49,9 @@ class MapFragment : Fragment() {
     ): View? {
 
         Mapbox.getInstance(this.requireContext(), getString(R.string.mapbox_access_token))
-        val binding: FragmentMapBinding = FragmentMapBinding.inflate(inflater)
-        map = binding.mapView
-        map.onCreate(savedInstanceState)
-
-        viewModel.mapMarkersLiveData.observe(viewLifecycleOwner, Observer { mapMarkers ->
-            setupMap(map, savedInstanceState, mapMarkers)
-        })
+        binding = FragmentMapBinding.inflate(inflater)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
 
         viewModel.goToBuildingDetailScreenLiveData.observe(viewLifecycleOwner, Observer {
             it.getContentIfNotHandled()?.let { buildingId ->
@@ -57,32 +63,46 @@ class MapFragment : Fragment() {
             }
         })
 
-        viewModel.loadMapData()
-
         binding.showBuildingListButton.setOnClickListener {
             findNavController().navigate(MapFragmentDirections.actionMapFragmentToBuildingListFragment())
         }
 
+        viewModel.mapMarkersLiveData.observe(viewLifecycleOwner, Observer { mapMarkers ->
+            setupMap(mapView, mapMarkers)
+        })
+
+        viewModel.loadMapData()
+
         return binding.root
+    }
+
+    private fun checkLocationPermissions() {
+        activityViewModel.locationPermission.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { locationPermissionGranted ->
+                if (locationPermissionGranted) {
+                    centreMapOnUser()
+                }
+            }
+        })
+
+        activityViewModel.checkLocationPermissions()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        map.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
-
 
     private fun setupMap(
         map: MapView,
-        savedInstanceState: Bundle?,
         mapMarkers: List<SymbolOptions>
     ) {
+
         map.getMapAsync { mapboxMap ->
             mapboxMap.setStyle(
                 Style.Builder()
                     .fromUri(getString(R.string.mapbox_style_url))
             ) { style ->
-
                 style.addImage(
                     ICON_ID,
                     BitmapUtils.getBitmapFromDrawable(resources.getDrawable(R.drawable.map_pin_icon))!!
@@ -102,7 +122,31 @@ class MapFragment : Fragment() {
                 symbolManager.textAllowOverlap = true
 
                 symbolManager.create(mapMarkers)
+
+                this.style = style
+                this.mapboxMap = mapboxMap
+                checkLocationPermissions()
             }
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun centreMapOnUser() {
+        val locationComponentOptions = LocationComponentOptions.builder(requireContext())
+            .foregroundDrawable(R.drawable.mapbox_marker_icon_default)
+            .bearingTintColor(R.color.vpi__bright_foreground_inverse_holo_light)
+            .build()
+
+        val locationComponentActivationOptions = LocationComponentActivationOptions
+            .builder(requireContext(), style)
+            .locationComponentOptions(locationComponentOptions)
+            .build()
+
+        val locationComponent = mapboxMap.locationComponent
+        locationComponent.activateLocationComponent(locationComponentActivationOptions)
+        locationComponent.isLocationComponentEnabled = true
+
+        locationComponent.setCameraMode(CameraMode.TRACKING, 0, 15.0, null, null, null)
+        locationComponent.renderMode = RenderMode.COMPASS
     }
 }
