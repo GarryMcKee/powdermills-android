001565d00eb8b1aaa4c78f48b017a9041ebb8e0b
garrymckee <gmk0232@gmail.com>
2021-01-29 01:05:23 +0000

    Integrate Mapbox permissions and centering on user location

--- app/src/main/java/com/garrymckee/powdermills/MainActivity.kt
+++ app/src/main/java/com/garrymckee/powdermills/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.garrymckee.powdermills
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener {
+
+    val viewModel: MainViewModel by viewModels()
+    lateinit var permissionsManager: PermissionsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -19,4 +30,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+        viewModel.shouldCheckLocationPermissions.observe(this, Observer {
+            it.getContentIfNotHandled()?.let { shouldCheckForLocationPermissions ->
+                if (shouldCheckForLocationPermissions) {
+                    checkLocationPermissions()
+                }
+            }
+        })
+
+        return super.onCreateView(name, context, attrs)
+
+    }
+
+    private fun checkLocationPermissions() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            viewModel.setLocationPermission(true)
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) =
+        Toast.makeText(this, R.string.location_permissions_rationale, Toast.LENGTH_LONG).show()
+
+    override fun onPermissionResult(granted: Boolean) = viewModel.setLocationPermission(granted)
 }
