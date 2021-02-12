ce09db0db15dbd5ba831a80fe66ce44d62cea63e
garrymckee <gmk0232@gmail.com>
2021-02-12 01:20:15 +0000

    Implement formatted terms and conditions with markdown

--- app/src/main/java/com/garrymckee/powdermills/ui/useragreement/UserAgreementFragment.kt
+++ app/src/main/java/com/garrymckee/powdermills/ui/useragreement/UserAgreementFragment.kt
@@ -1,6 +1,9 @@
 package com.garrymckee.powdermills.ui.useragreement
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.garrymckee.powdermills.R
 import com.garrymckee.powdermills.databinding.FragmentUserAgreementBinding
 import com.garrymckee.powdermills.domain.useragreement.UserAgreement
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,6 +35,8 @@ class UserAgreementFragment : Fragment() {
             viewModel.updateUserAgreement(UserAgreement(true))
         }
 
+        setupTermsAndConditionText()
+
         viewModel.continueToMapScreen.observe(viewLifecycleOwner, Observer {
             it.getContentIfNotHandled()?.let {
                 continueToNextScreen()
@@ -38,6 +44,26 @@ class UserAgreementFragment : Fragment() {
         })
 
         return binding.root
+    }
+
+    private fun setupTermsAndConditionText() {
+        /*
+        The terms and conditions text uses some simple markup, we use the Html.fromHTML method to achieve this.
+        For backwards compatibility a version check is required and will back to the older deprecated method if required.
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            binding.termsAndConditionsTextView.text =
+                Html.fromHtml(
+                    resources.getString(R.string.terms_and_conditions_html),
+                    Html.FROM_HTML_MODE_COMPACT
+                )
+        } else {
+            binding.termsAndConditionsTextView.text = Html.fromHtml(
+                resources.getString(R.string.terms_and_conditions_html)
+            )
+        }
+
+        binding.termsAndConditionsTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 
     private fun continueToNextScreen() {
